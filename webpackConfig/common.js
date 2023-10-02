68a659e1a5aa6a890546fa3a0bcbb8c7f05c0f8e
Calorific <kknd3390@mail.ru>
2023-10-03 00:51:19 +0700

    feat: добавляет выбор звуков погоды

---
+++ webpackConfig/common.js
@@ -0,0 +1,62 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  stats: 'errors-warnings',
+  context: path.resolve(__dirname, '..', 'src'),
+  entry: './index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, '..', 'dist'),
+    clean: true
+  },
+  resolve : {
+    extensions: ['.tsx', '.ts', '.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../src/index.html'),
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../public'),
+          to: path.resolve(__dirname, '../dist')
+        }
+      ]
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [require('postcss-preset-env')]
+            }
+          }
+        }]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [require('postcss-preset-env')]
+            }
+          }
+        }, 'sass-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|png|jpe?g|gif|svg|mp3)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  }
+}
