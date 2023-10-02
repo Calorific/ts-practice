84d8a4a0210b56a47b25d8b84cc7342f110dbc73
Calorific <kknd3390@mail.ru>
2023-10-03 01:59:45 +0700

    fix: разбивает медиа на 2 папки, чтобы не было дубликтов

--- webpackConfig/common.js
+++ webpackConfig/common.js
@@ -22,7 +22,7 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, '../public'),
+          from: path.resolve(__dirname, '../assets'),
           to: path.resolve(__dirname, '../dist')
         }
       ]
